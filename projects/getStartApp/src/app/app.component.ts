e98e977eb30f88f841d6a2467dba6fd3dd324dbc
Ron_Jr <ronaldoj.w2007@gmail.com>
2020-02-28 18:10:38 -0300

    create doc to lib

--- projects/getStartApp/src/app/app.component.ts
+++ projects/getStartApp/src/app/app.component.ts
@@ -1,10 +1,21 @@
-import { Component } from '@angular/core';
+import { Component, ViewContainerRef, QueryList, ViewChildren } from '@angular/core';
+import { CreatedComponents } from './../components'
+import { DomSanitizer } from '@angular/platform-browser';
+
+@ViewChildren('dynamic', {read: ViewContainerRef}) 
+
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
+
 export class AppComponent {
+  public widgetTargets: QueryList<ViewContainerRef>;
   title = 'getStartApp';
+  htmlStr: string = '<strong>The Tortoise</strong> &amp; the Hare';
+  components = CreatedComponents.components;
+
+  constructor(private sanitizer: DomSanitizer) {}
 }
--- projects/getStartApp/src/app/app.module.ts
+++ projects/getStartApp/src/app/app.module.ts
@@ -1,14 +1,16 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
 import { AppComponent } from './app.component';
+import { ExampleComponentModule } from 'example-component';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    ExampleComponentModule
+    
   ],
   providers: [],
   bootstrap: [AppComponent]
