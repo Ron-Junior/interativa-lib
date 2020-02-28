118873f72fa4a9b228e038def6cf0d02c8ad6d39
Ron_Jr <ronaldoj.w2007@gmail.com>
2020-02-28 18:11:28 -0300

    create component

---
+++ projects/getStartApp/src/components.ts
@@ -0,0 +1,14 @@
+export class CreatedComponents {
+    public static components = [
+        {
+            tag: '<lib-exampleComponent></lib-exampleComponent>', // tags to access components
+            name: 'interativa-lib-example-component', // name of component ex: name-of-library-name-of-component, in this case name of library is interativaLib and name of compoent is exampleComponent 
+            versions: [ // add on final array new version created for you
+                {
+                    number: '0.0.1'  // version finded in package.json on each libraries
+                }
+            ],
+            
+        }
+    ]
+}
